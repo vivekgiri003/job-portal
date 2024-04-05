f5a9f37272731df1748d8d5289ed75fb7dd1a6d2
Vivek Giri <111175971+vivekgiri003@users.noreply.github.com>
2024-04-05 20:06:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:8000"
+                url: "https://job-portal-suk5.onrender.com"
             }
         ]
     },
